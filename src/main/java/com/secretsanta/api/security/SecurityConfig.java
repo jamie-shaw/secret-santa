5009476a7f632195388d212249de6aae8ff36760
Jamie Shaw <jamie.e.shaw@gmail.com>
2019-10-19 18:27:28 -0400

    removed h2 support

--- src/main/java/com/secretsanta/api/security/SecurityConfig.java
+++ src/main/java/com/secretsanta/api/security/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/images/**").permitAll()
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/changePassword").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
