455b6b4b502007ff8ffe9581739130efff560ca6
Jamie Shaw <jamie.e.shaw@gmail.com>
2019-11-02 09:56:10 -0400

    Refactor data access

--- src/main/java/com/secretsanta/api/controller/EmailController.java
+++ src/main/java/com/secretsanta/api/controller/EmailController.java
@@ -3,26 +3,26 @@ package com.secretsanta.api.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.client.RestTemplate;
 
-import com.secretsanta.api.mapper.UserMapper;
+import com.secretsanta.api.dao.UserDao;
 import com.secretsanta.api.model.User;
+import com.secretsanta.api.service.EmailService;
 
 @Controller
 @SessionAttributes({"CURRENT_USER", "RECIPIENT"})
 public class EmailController extends BaseController {
     
-    private static final String uri = "http://www.pmshockey.com/wp-admin/admin-ajax.php?";
+    @Autowired
+    private UserDao userDao;
     
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private EmailService emailService;
     
     @GetMapping("/email")
     public String showEmail() {
@@ -34,6 +34,7 @@ public class EmailController extends BaseController {
                                HttpServletRequest request,
                                Model model) {
         
+        String message = request.getParameter("message");
         String filterColumn;
         String subject;
         
@@ -47,23 +48,11 @@ public class EmailController extends BaseController {
             subject = "A message from your Secret Santa recipient";
         }
             
-        String SQL = "SELECT santa_user.user_name, display_name, email " +
-                       "FROM " + getSchema() + ".recipient " + 
-                 "INNER JOIN " + getSchema() + ".santa_user ON recipient.recipient = santa_user.user_name " +
-                      "WHERE recipient." + filterColumn + " = ? AND Year = ?";
-
-        User user = jdbcTemplate.queryForObject(SQL, new Object[]{currentUser, getCurrentYear()}, new UserMapper());
+        User user = userDao.getUser(currentUser, filterColumn);
         
-        String message = request.getParameter("message");
-
-        RestTemplate restTemplate = new RestTemplate();
+        emailService.sendEmail(user.getEmail(), subject, message);
         
-        restTemplate.getForEntity(uri + "to=jamie.e.shaw@gmail.com&"
-                                      + "subject=" + subject + "&"
-                                      + "message=" + message + "&"
-                                      + "action=send_email&", String.class);
-        
-       setSuccessMessage(request, "Your message has been sent.");
+        setSuccessMessage(request, "Your message has been sent.");
        
         return "redirect:/email";
         
---
+++ src/main/java/com/secretsanta/api/service/EmailService.java
@@ -0,0 +1,20 @@
+package com.secretsanta.api.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class EmailService {
+    
+    private static final String URI = "http://www.pmshockey.com/wp-admin/admin-ajax.php?";
+    
+    RestTemplate restTemplate = new RestTemplate();
+    
+    public void sendEmail(String address, String subject, String message) {
+        
+        restTemplate.getForEntity(URI + "to=jamie.e.shaw@gmail.com&"
+                                      + "subject=" + subject + "&"
+                                      + "message=" + message + "&"
+                                      + "action=send_email&", String.class);
+    }
+}
