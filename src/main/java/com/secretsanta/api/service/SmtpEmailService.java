51c2cb7336530b3720b7f9f2ab1bcd65b2d7c177
Jamie Shaw <jamie.e.shaw@gmail.com>
2022-11-19 16:27:42 -0500

    Add support for SMTP email notifications

---
+++ src/main/java/com/secretsanta/api/service/SmtpEmailService.java
@@ -0,0 +1,55 @@
+package com.secretsanta.api.service;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.thymeleaf.context.Context;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class SmtpEmailService extends BaseEmailService implements EmailService {
+    
+    @Autowired 
+    private JavaMailSender javaMailSender;
+    
+    RestTemplate restTemplate = new RestTemplate();
+    
+    public void sendEmail(String destinationAddress, String subject, String templateName, Context templateContext) {
+        
+        // override destination address if necessary
+        destinationAddress = getFinalDestinationAddress(destinationAddress);
+        
+        // override the subject
+        subject = getFinalSubject(subject);
+        
+        // build the content
+        String message = buildEmailBody(templateName, templateContext);
+        
+        try {
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            
+            MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+            
+            helper.setFrom("Secret Santa <jamie.e.shaw@gmail.com>");
+            helper.setTo(destinationAddress);
+            helper.setText(message, true);
+            helper.setSubject(subject);
+            
+            javaMailSender.send(msg);
+            
+            log.info("Mail Sent Successfully...");
+        }
+ 
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            log.error("SMTP error", e);
+        }
+    }
+
+}
