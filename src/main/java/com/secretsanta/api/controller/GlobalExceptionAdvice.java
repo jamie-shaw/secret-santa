b4e971ef87eb11bedfd4c862fac4b8fa3f2dfcbc
Jamie Shaw <jamie.e.shaw@gmail.com>
2022-11-20 18:55:54 -0500

    improve error handling

--- src/main/java/com/secretsanta/api/controller/GlobalExceptionAdvice.java
+++ src/main/java/com/secretsanta/api/controller/GlobalExceptionAdvice.java
@@ -3,11 +3,17 @@ package com.secretsanta.api.controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionAdvice {
    
     @ExceptionHandler(Exception.class)
     public String handleError(Exception e) {
+       
+       log.error("GlobalExceptionAdvice", e);
+       
        return "error-page";
      }
 
