2563868eb729de666818083a26f3be001261fc5e
Jamie Shaw <jamie.e.shaw@gmail.com>
2025-12-02 19:13:02 -0500

    Add prettier and reformat

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ApiStatusComponent } from "./pages/api-status/api-status.component";
+import { LoginComponent } from "./pages/login/login.component";
 
-const routes: Routes = [{ path: "", component: ApiStatusComponent }];
+const routes: Routes = [
+    { path: "", component: LoginComponent },
+    { path: "api-status", component: ApiStatusComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
