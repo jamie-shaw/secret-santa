46af469082f4f79f9bb207ed2ffe2a11dd64c825
Jamie Shaw <jamie.e.shaw@gmail.com>
2025-12-05 19:34:35 -0500

    rough in home component

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: "app-home",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./home.component.html",
+    styleUrl: "./home.component.css",
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
