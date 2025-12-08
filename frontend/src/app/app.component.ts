f87cfdaa7ad19076c805f542c5a888c8c4822139
Jamie Shaw <jamie.e.shaw@gmail.com>
2025-12-07 22:31:59 -0500

    add current year logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = "Secret Santa";
+export class AppComponent implements OnInit {
+    title = "Secret Santa";
+
+    constructor() {}
+
+    ngOnInit(): void {}
 }
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from "@angular/core";
+import { getCurrentYear } from "../../utils/globals";
 
 @Component({
-  selector: "app-header",
-  standalone: true,
-  imports: [],
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.css",
+    selector: "app-header",
+    standalone: true,
+    imports: [],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.css",
 })
 export class HeaderComponent {
-  @Input() title: string = "Secret Santa";
+    @Input() title: string = "Secret Santa";
+
+    currentYear = getCurrentYear();
 }
