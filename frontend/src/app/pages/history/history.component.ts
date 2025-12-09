b461b687d7bbc5e1dbb8c5511fa8ca8356ada72a
Jamie Shaw <jamie.e.shaw@gmail.com>
2025-12-08 23:26:41 -0500

    implement history page

--- frontend/src/app/pages/history/history.component.ts
+++ frontend/src/app/pages/history/history.component.ts
@@ -1,39 +1,38 @@
-import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { RecipientService } from "../../services/recipient.service";
 import { Recipient } from "src/app/models/recipient.model";
 import { CommonModule } from "@angular/common";
-import { getCurrentYear } from "../../utils/globals";
+import { FormsModule } from "@angular/forms";
 import { combineLatest, map, BehaviorSubject } from "rxjs";
 
 @Component({
     selector: "app-history",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: "./history.component.html",
     styleUrl: "./history.component.css",
 })
 export class HistoryComponent {
     recipients: Recipient[] = [];
     years: number[] = [];
+    selectedYear: number = 0;
+    error: string | null = null;
 
-    private recipientsLoading$ = new BehaviorSubject<boolean>(true);
-    private yearsLoading$ = new BehaviorSubject<boolean>(true);
+    recipientsLoading$ = new BehaviorSubject<boolean>(true);
+    yearsLoading$ = new BehaviorSubject<boolean>(true);
+
     loading$ = combineLatest([this.recipientsLoading$, this.yearsLoading$]).pipe(
         map(([recipientsLoading, yearsLoading]) => recipientsLoading || yearsLoading),
     );
 
-    error: string | null = null;
-    selectedYear: number = getCurrentYear();
-
     constructor(private recipientService: RecipientService) {}
 
     ngOnInit() {
-        this.error = null;
-
-        this.getAllRecipientsForYear(this.selectedYear);
-
         this.getHistoryYears();
+    }
+
+    onYearChange(year: number) {
+        this.getAllRecipientsForYear(year);
     }
 
     private getHistoryYears() {
@@ -41,34 +40,30 @@ export class HistoryComponent {
             next: (years) => {
                 this.years = years;
                 this.yearsLoading$.next(false);
+
+                // Set selectedYear to the most recent year
+                this.selectedYear = this.years[0];
+                this.getAllRecipientsForYear(this.selectedYear);
             },
-            error: (err) => {
-                this.error = "Failed to get history data";
-                this.recipientsLoading$.next(false);
-                this.yearsLoading$.next(false);
-            },
+            error: (err) => this.handleError(err),
         });
     }
 
-    onYearChange(event: Event) {
-        const target = event.target as HTMLSelectElement;
-        this.selectedYear = Number(target.value);
-        this.recipientsLoading$.next(true);
-        this.getAllRecipientsForYear(this.selectedYear);
-    }
-
     private getAllRecipientsForYear(year: number) {
+        this.recipientsLoading$.next(true);
         this.recipientService.getAllRecipients(year).subscribe({
             next: (recipients) => {
                 this.recipients = recipients;
                 this.recipientsLoading$.next(false);
             },
-            error: (err) => {
-                this.error = "Failed to get history data";
-                this.recipientsLoading$.next(false);
-                this.yearsLoading$.next(false);
-                console.error("API Error:", err);
-            },
+            error: (err) => this.handleError(err),
         });
+    }
+
+    private handleError(err: any) {
+        this.error = "Failed to get history data";
+        this.recipientsLoading$.next(false);
+        this.yearsLoading$.next(false);
+        console.error("API Error:", err);
     }
 }
