b488e064afe1d75223e571e7fe28a19fecfca74d
Jamie Shaw <jamie.e.shaw@gmail.com>
2025-12-22 09:58:24 -0500

    add days remaining

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -12,10 +12,27 @@ import { Recipient } from "src/app/models/recipient.model";
 })
 export class HomeComponent {
     recipient: Recipient | null = null;
+    public daysRemaining: number = 0;
 
     constructor(private recipientService: RecipientService) {}
 
     ngOnInit() {
         this.recipient = this.recipientService.currentRecipient;
+        this.daysRemaining = this.calculateDaysUntilChristmas();
+    }
+
+    private calculateDaysUntilChristmas(): number {
+        const today = new Date();
+        const currentYear = today.getFullYear();
+        const christmas = new Date(currentYear, 11, 25); // December 25th
+
+        if (today > christmas) {
+            // If today is after Christmas, calculate for next year
+            christmas.setFullYear(currentYear + 1);
+        }
+
+        const diffTime = christmas.getTime() - today.getTime();
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays;
     }
 }
